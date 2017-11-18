abce7523f48634d61d33c61eee6d82db35ff4ae3
alirezafiroozi <alireza.firoozi68@gmail.com>
2017-11-18 14:49:32 -0800

    system.cpp

--- untitled/system.cpp
+++ untitled/system.cpp
@@ -7,18 +7,19 @@ System::System()
 {
     cout << "System CTOR got called\n";
     Planet planet;
-//    float xe = 809.6 /** pow(10, 6)*/;
-//    float ye = 50/*pow(10, 6)*/;
-//    sf::Vector2f pos(xe, ye);
-//    float masse = 5.97 * pow(10,3);
-//    sf::Color cole(sf::Color::Blue);
-//    Planet Earth(pos, 3, 2, masse, 20, cole);
+    float xe = 8.6 /** pow(10, 6)*/;
+    float ye = 50/*pow(10, 6)*/;
+    sf::Vector2f pos(xe, ye);
+    sf::Vector2f vel(1, 1);
+    float masse = 5.97 * pow(10,3);
+    sf::Color cole(sf::Color::Blue);
+    Planet Earth(pos, vel, masse, 20, cole);
 ////    float xe = 149.6 * pow(10, 6);
 ////    float ye = pow(10, 6);
 //    float masss = pow(10, 5);
 //    sf::Color cols(sf::Color::Yellow);
 //    Planet Sun(0, 0, 0, 0, masss, 50, cols);
-////    planetVector.push_back(Earth);
+    planetVector.push_back(Earth);
 //    planetVector.push_back(Sun);
 
     //NEEDS TO GET COMPLETED
