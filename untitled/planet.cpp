14d8766109ded7326a3bbbf272a5163a65c8fc1a
alirezafiroozi <alireza.firoozi68@gmail.com>
2017-11-18 16:07:05 -0800

    collision done

--- untitled/planet.cpp
+++ untitled/planet.cpp
@@ -64,30 +64,48 @@ Planet Planet::Acceleration(sf::Vector2f force)
 
 void Planet::Collision(Planet& other)
 {
-
     ////EQUATION: gm*sum of(m_i/delta-distance_i)*(direction)
     /// i = each particle
-    double d = sqrt(pow(_pos.x - other._pos.x, 2)
-                    + pow(_pos.y - other._pos.y, 2));
-    if(d < (_radius + other._radius))
+    ///
+    float tempX = 0.0;
+    float tempY = 0.0;
+    if(_mass >= other._mass)
     {
-        if(_mass > other._mass)
-        {
-            _mass += other._mass / 10;
-            _vel.x += other._vel.x / other._mass;
-            _vel.y += other._vel.y / other._mass;
-            other._alive = false;;
-        }
-        if(_mass <= other._mass)
-        {
-            other._mass += _mass / 10;
-            other._vel.x += _vel.x / _mass;
-            other._vel.y += _vel.y / _mass;
-
-            _alive = false;;
-        }
-
+        tempX = _mass * pow(_vel.x, 2) + other._mass * pow(other._vel.x, 2);
+        tempY = _mass * pow(_vel.y, 2) + other._mass * pow(other._vel.y, 2);
+        _vel.x = sqrt(tempX / (_mass + other._mass));
+        _vel.y = sqrt(tempY / (_mass + other._mass));
+        other._alive = false;
     }
+    if(_mass < other._mass)
+    {
+        tempX = _mass * pow(_vel.x, 2) + other._mass * pow(other._vel.x, 2);
+        tempY = _mass * pow(_vel.y, 2) + other._mass * pow(other._vel.y, 2);
+        other._vel.x = sqrt(tempX / (_mass + other._mass));
+        other._vel.y = sqrt(tempY / (_mass + other._mass));
+        _alive = false;
+    }
+
+    //    double d = sqrt(pow(_pos.x - other._pos.x, 2)
+//                    + pow(_pos.y - other._pos.y, 2));
+//    if(d < (_radius + other._radius))
+//    {
+//        if(_mass > other._mass)
+//        {
+//            _mass += other._mass / 10;
+//            _vel.x += other._vel.x / other._mass;
+//            _vel.y += other._vel.y / other._mass;
+//            other._alive = false;;
+//        }
+//        if(_mass <= other._mass)
+//        {
+//            other._mass += _mass / 10;
+//            other._vel.x += _vel.x / _mass;
+//            other._vel.y += _vel.y / _mass;
+
+//            _alive = false;;
+//        }
+//    }
 }
 
 bool Planet::IsAlive()
